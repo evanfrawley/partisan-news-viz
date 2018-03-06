3f77feffd62f63884bd33faa5ec42e173a773fbb
evanfrawley <frawley@uw.edu>
2018-03-06 13:59:33 -0800

    fix graph bug

--- components/graph.js
+++ components/graph.js
@@ -31,11 +31,11 @@ const d3 = require('d3');
 </div>
 
 
-<link 
-    rel="stylesheet" 
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
-    crossorigin="anonymous"> 
+<link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+    crossorigin="anonymous">
 <script
   src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
   integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
@@ -45,28 +45,28 @@ const d3 = require('d3');
 <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
 */
 class Graph extends D3Component {
-    
+
     redraw = () => {
 
         console.log("redrawn");
-        
+
         var totalIndividuals = 0; // for chart viz
         var activeNode = -1; // for hover effect
-        
+
 
         d3.json(`https://jessecoleman.github.io/sir-rumor-viz/data_${this.modelParams.density.default}_${this.modelParams.cluster.default}.json`, (error, graph_data) => {
         //d3.json(`data_${this.modelParams.density.default}_${this.modelParams.cluster.default}.json`, function(error, graph_data) {
             if (error) throw error;
             d3.select("#rumor-list").selectAll("svg").remove();
 
-            this.x = d3.scaleLinear()
+            let x = d3.scaleLinear()
                 .domain([-1, 1])
                 .range([0, this.width]);
-    
-            this.y = d3.scaleLinear()
+
+            let y = d3.scaleLinear()
                 .domain([-1, 1])
                 .range([0, this.height]);
-    
+
             graph_data.nodes.forEach((node) => {
                 totalIndividuals++;
                 node.rumors = {};
@@ -77,51 +77,51 @@ class Graph extends D3Component {
                 node.x = +node.x;
                 node.y = +node.y;
             });
-        
+
             graph_data.links.forEach((l) => {
                 graph_data.nodes[l.target].neighbors.push(l.source);
             });
-        
-            var links = linkGroup.selectAll("line")
+
+            var links = this.linkGroup.selectAll("line")
                 .data(graph_data.links)
-    
+
             console.log(6);
             links.enter().append("line")
                 .merge(links)
                 .attr("class", "links")
-                .attr("x1", function(d) { return this.x(graph_data.nodes[d.source].x); })
-                .attr("y1", function(d) { return this.y(graph_data.nodes[d.source].y); })
-                .attr("x2", function(d) { return this.x(graph_data.nodes[d.target].x); })
-                .attr("y2", function(d) { return this.y(graph_data.nodes[d.target].y); })
+                .attr("x1", function(d) { return x(graph_data.nodes[d.source].x); })
+                .attr("y1", function(d) { return y(graph_data.nodes[d.source].y); })
+                .attr("x2", function(d) { return x(graph_data.nodes[d.target].x); })
+                .attr("y2", function(d) { return y(graph_data.nodes[d.target].y); })
                 .attr("stroke", "#ccc")
                 .attr("stroke-width", 1)
                 .attr("opacity", 0)
                 .transition().delay(500).duration(100)
                 .attr("opacity", 1)
-    
+
             links.exit().remove()
-    
+
             console.log(7);
             var radius = d3.scaleSqrt()
                 .range([4, 7])
                 .domain(d3.extent(graph_data.nodes, function(node) { return node.rank; }));
-    
-            var nodes = nodeGroup
+
+            var nodes = this.nodeGroup
                 .selectAll("circle")
                 .data(graph_data.nodes);
-    
+
             nodes.enter().append("circle")
                 .merge(nodes)
                 .transition()
                 .duration(500)
                 .attr("class", "nodes")
                 .attr("r", function(d) { return radius(d.rank); })
-                .attr("cx", function(d) { return this.x(d.x); })
-                .attr("cy", function(d) { return this.y(d.y); })
+                .attr("cx", function(d) { return x(d.x); })
+                .attr("cy", function(d) { return y(d.y); })
                 .attr("fill", function(d) { return d3.interpolateReds(0); })
                 .style("stroke", "#666")
-    
-            nodeGroup.selectAll("circle")
+
+            this.nodeGroup.selectAll("circle")
                 .on("click", infect)
                 .on("mouseover", function(node) {
                     activeNode = node.id;
@@ -131,12 +131,12 @@ class Graph extends D3Component {
                     tooltip.html(function(d) {
                         var text = "<ul>";
                         Object.keys(node.rumors).reverse().forEach(function(rumor) {
-                            text += "<li class=\"" + 
-                                node.rumors[rumor] + 
-                                "\">" + rumor + " " + 
+                            text += "<li class=\"" +
+                                node.rumors[rumor] +
+                                "\">" + rumor + " " +
                                 node.rumors[rumor] + "</li>";
                         });
-                        return text; 
+                        return text;
                     })
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY) + "px");
@@ -147,7 +147,7 @@ class Graph extends D3Component {
                         .duration(200)
                         .style("opacity", 0);
                 });
-        
+
         });
     }
 
@@ -159,8 +159,8 @@ class Graph extends D3Component {
         this.svg = d3.select(node).append("svg")
             .attr("width", this.width)
             .attr("height", this.height);
-    
-    
+
+
         this.modelParams = {
             "density": {
                 "min": 0.01,
@@ -194,7 +194,7 @@ class Graph extends D3Component {
                 "default": 0.2
             }
         };
-    
+
         this.linkGroup = this.svg.append("g");
         this.nodeGroup = this.svg.append("g");
 
@@ -203,27 +203,27 @@ class Graph extends D3Component {
         var tooltip = d3.select("body").append("div")
             .attr("class", "tooltip");
     }
-       
+
     startInterval = () => {
         d3.interval(function() {
             iter++;
-        
+
             //while (infecting) {}
-        
+
             // interaction between nodes and neighbors
             this.nodes.forEach(function(node) {
-            
+
                 //neighbor = graph.nodes[node.neighbors[Math.floor(Math.random()*node.neighbors.length)]];
-            
+
                 node.neighbors.forEach(function(n) {
                     neighbor = graph_data.nodes[n];
-                
+
                     // probability of interacting decreases with number of previous interactions
                     if (1 / Math.pow(Object.keys(neighbor.rumors).length + 1, 2) > Math.random()) {
-                    
+
                         var spread = false;
                         var rumorAge = 1;
-                        var r = Math.random();                        
+                        var r = Math.random();
                         Object.keys(node.rumors).sort().reverse().forEach(function(rumor) {
                             // probability of sharing rumor given age
                             if (1 - (1 / Math.pow(2, rumorAge)) < r && !spread) {
@@ -246,7 +246,7 @@ class Graph extends D3Component {
                     }
                 });
             });
-        
+
             Object.keys(rumorStates).forEach(function(r) {
                 rumorStates[r].ignorant = totalIndividuals;
                 rumorStates[r].spreading = 0;
@@ -254,17 +254,17 @@ class Graph extends D3Component {
             })
             // update graph data
             graph_data.nodes.forEach(function(node) {
-            
+
                 Object.keys(node.newRumors).forEach(function(rumor) {
                     node.rumors[rumor] = node.newRumors[rumor];
                 });
                 node.newRumors = {};
-            
+
                // count number of active rumors
                 node.spreading = 0;
                 node.stifling = 0;
                 Object.keys(node.rumors).forEach(function(r) {
-                
+
                     if (node.rumors[r] == "spreader") {
                         rumorStates[r].spreading++;
                         rumorStates[r].ignorant--;
@@ -276,30 +276,30 @@ class Graph extends D3Component {
                         node.stifling += 1 / Math.pow(rumor - r, 2);
                     }
                 });
-            
+
                 if (node.id == activeNode) {
                     tooltip.html(function(d) {
                         var text = "<ul>";
                         Object.keys(node.rumors).reverse().forEach(function(rumor) {
-                            text += "<li class=\"" + node.rumors[rumor] + "\">" + 
+                            text += "<li class=\"" + node.rumors[rumor] + "\">" +
                                 rumor + " " + node.rumors[rumor] + "</li>";
                         })
-                        return text; 
+                        return text;
                     })
                 }
             });
-        
+
             setNodeFill(nodes);
-        
+
             // update chart data
             Object.keys(charts).forEach(function(r) {
-            
+
                 chart = charts[r];
-            
+
                 chart.data.ignorant.push({"iter": iter, "count": rumorStates[r].ignorant});
                 chart.data.spreading.push({"iter": iter, "count": rumorStates[r].spreading});
                 chart.data.stifling.push({"iter": iter, "count": rumorStates[r].stifling});
-            
+
                 chart.x.domain([iter - 20, iter]);
                 chart.axisBottom
                     .call(d3.axisBottom(chart.x).ticks(10))
@@ -312,25 +312,25 @@ class Graph extends D3Component {
                     .attr("dy", "0.71em")
                     .attr("text-anchor", "end")
                     .text("Ratios");
-            
+
                 chart.lineIgnorant
                     .datum(chart.data.ignorant)
                     .attr("d", chart.line);
-            
+
                 chart.lineSpreading
                     .datum(chart.data.spreading)
                     .attr("d", chart.line);
-            
+
                 chart.lineStifling
                     .datum(chart.data.stifling)
                     .attr("d", chart.line);
             });
-        
+
         }, 1000);
     }
-        
+
     setNodeFill(nodes) {
-        nodes.style("fill", function(node) { 
+        nodes.style("fill", function(node) {
             if (selectedRumor == -1) {
                 return d3.interpolateReds(Math.pow(1 - (1 / Math.pow(2, node.spreading)), 2));
             } else if (node.rumors[selectedRumor] == "spreader") {
