d8d417d6be5b880a190420d70b83da449b9649f5
evanfrawley <frawley@uw.edu>
2018-03-06 13:05:30 -0800

    Add mathjax support

--- components/side-panel.js
+++ components/side-panel.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import CustomD3Component from "./custom-d3-component";
+import FlowChart from "./flow-chart.js";
+import MathJaxComponent from "./math-jax-component.js";
+
+const equation1Text = String.raw`
+$$\cos (2\theta) = \cos^2 \theta - \sin^2 \theta$$
+
+$$\cos (2\theta) = \cos^2 \theta - \sin^2 \theta$$
+
+$$\lim_{x \to \infty} \exp(-x) = 0$$
+
+$$k_{n+1} = n^2 + k_n^2 - k_{n-1}$$
+
+$$k_{n+1} = n^2 + k_n^2 - k_{n-1}$$
+`;
+
+const equation2Text = String.raw`
+$$\cos (2\theta) = \cos^2 \theta - \sin^2 \theta$$
+`;
 
 class SidePanel extends React.Component {
   constructor() {
@@ -12,16 +30,20 @@ class SidePanel extends React.Component {
     let view = this.props.view;
     switch(view) {
       case 0: {
-        return (<div>none</div>)
+        return (
+            <div>0</div>
+        )
       }
       case 1: {
-        return (<div>first</div>)
+        return (
+          <FlowChart />
+        )
       }
       case 2: {
-        return (<div>second</div>)
+        return (<MathJaxComponent tex={equation1Text} />)
       }
       case 3: {
-        return (<div>third</div>)
+        return (<MathJaxComponent tex={equation2Text} />)
       }
       default: {
         return (
